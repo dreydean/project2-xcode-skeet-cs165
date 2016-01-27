ba92ebb81141dcac2aea614d880d11dbe853b987
Drey Platt <dreyplatt@gmail.com>
2016-01-26 18:00:48 -0700

    Initial Commit

---
+++ project2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project2
+//
+//  Created by Drey Platt on 1/26/16.
+//  Copyright (c) 2016 Drey Platt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
